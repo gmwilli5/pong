02acc7d0bba8a1eef837a4a132c362368aafa781
gmwilli5 <gmwilli5@ncsu.edu>
2013-08-12 01:15:56 -0400

    forgot to add new files to last commit added structs and started on paddle and ball classes

---
+++ paddle.cpp
@@ -0,0 +1,15 @@
+#include "paddle.h"
+void paddle::re_load_paddle()
+{
+    load_paddle();
+}
+void paddle::move(directions direction)
+{
+    if(direction!=up&&direction!=down){
+        return;
+    }
+}
+void paddle::load_paddle()
+{
+
+}
---
+++ structs.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTS_H_INCLUDED
+#define STRUCTS_H_INCLUDED
+struct vec2{
+    short int x;
+    short int y;
+};
+enum directions{
+    up,
+    down,
+    left,
+    right
+};
+#endif // STRUCTS_H_INCLUDED

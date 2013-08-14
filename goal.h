0753a5a94102931a77cdbe56540cbd785b1cf5d6
gmwilli5 <gmwilli5@ncsu.edu>
2013-08-14 00:45:06 -0400

    started collision detection compiles now added player namespace and wall and goal classes for for collision detection

---
+++ goal.h
@@ -0,0 +1,10 @@
+#ifndef GOAL_H_INCLUDED
+#define GOAL_H_INCLUDED
+class goal{
+    private:
+        aabb position;
+    public:
+        void get_position();
+        void update();
+};
+#endif // GOAL_H_INCLUDED
---
+++ wall.h
@@ -0,0 +1,10 @@
+#ifndef WALL_H_INCLUDED
+#define WALL_H_INCLUDED
+class wall{
+    private:
+        aabb position;
+    public:
+        void get_position();
+        void update();
+};
+#endif // WALL_H_INCLUDED
